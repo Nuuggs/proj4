e5a5f4c0a8a8edcec6d46f5dbe9cdf2a3e8fbe25
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2022-01-24 11:02:09 +0800

    added friends qeury

--- routers/userRouter.mjs
+++ routers/userRouter.mjs
@@ -11,5 +11,6 @@ const userCtrl = new UserCtrl('main', db.User, db);
 router.post('/register', userCtrl.postRegister.bind(userCtrl));
 router.post('/login', userCtrl.postLogin.bind(userCtrl));
 router.post('/email', userCtrl.postEmail.bind(userCtrl));
+router.post('/friends', userCtrl.getUsers.bind(userCtrl));
 
 export default router;
